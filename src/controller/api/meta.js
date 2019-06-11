359ed47b9408e17ccceccc0e70aeeac2a57e8c2b
xing.sun <xing.sun@nio.com>
2019-06-11 18:44:32 +0800

    删除多余文件

--- src/controller/api/meta.js
+++ src/controller/api/meta.js
@@ -19,7 +19,7 @@ module.exports = class extends BaseRest {
             data = await this.modelInstance
                 .where(params)
                 .page(page, pageSize)
-                .order("sort desc")
+                .order("create_time desc")
                 .countSelect();
             return this.success(data);
         }
@@ -28,5 +28,42 @@ module.exports = class extends BaseRest {
             .order("sort desc")
             .select();
         return this.success(data);
+    }
+    async postAction() {
+        const name = this.post("name"),
+            type = this.post("type"),
+            id = this.post("id"),
+            date = this.post("date");
+        if (id) {
+            const res = await this.modelInstance
+                .where({ id })
+                .update({ name, type, update_time: date });
+            if (res) {
+                this.success({ msg: "修改标签成功!" });
+            }
+        } else {
+            //新增
+            const newId = await this.modelInstance.add({
+                name,
+                type,
+                count: 0,
+                sort: 0,
+                user_id: 1,
+                create_time: date
+            });
+            if (newId) {
+                this.success({ msg: "新增标签成功!" });
+            }
+        }
+    }
+    async deleteAction() {
+        const id = this.post("id");
+        const affectedRows = this.modelInstance.where({ id }).delete();
+        if (affectedRows) {
+            // this.fail(1000, "删除标签失败");
+            return this.success(affectedRows);
+        } else {
+            this.fail(1000, "删除标签失败");
+        }
     }
 };
